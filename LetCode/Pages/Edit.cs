391188343f03abecde6464c27fd251928b00a72e
AkSwaiti <ak.swaiti@gmail.com>
2025-12-09 17:07:33 +0300

    last update

---
+++ LetCode/Pages/Edit.cs
@@ -0,0 +1,55 @@
+﻿using LetCode.Utils;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetCode.Pages
+{
+     class Edit
+    {
+        private IWebDriver driver;
+        private CustomMethod custom;
+        public Edit(IWebDriver driver)
+        {
+            this.driver = driver;
+            this.custom = new CustomMethod(driver);
+        }
+        private IWebElement Name => driver.FindElement(By.Id("fullName"));
+        private IWebElement TextAndTab => driver.FindElement(By.Id("join"));
+        private IWebElement ClearText => driver.FindElement(By.Id("clearMe"));
+        private IWebElement DisabledField => driver.FindElement(By.Id("noEdit"));
+        private IWebElement ReadOnly => driver.FindElement(By.Id("dontwrite"));
+        public void EnterFullName(string fullName)
+        {
+            custom.Type(Name, fullName);
+        }
+        public void AddText(string anyText)
+        {
+            custom.Type(TextAndTab, anyText);
+            custom.PressOn("Tab");
+
+        }
+        public void RemoveText(IWebElement element)
+        {
+            element = ClearText;
+            element.Clear();
+        }
+        public  void CheckFieldIfItsEnabled(IWebElement element)
+        {
+            element = DisabledField;
+            custom.IsItEnabled(element);
+        }
+        public void CheckFieldIfItsReadOnly(IWebElement element, string value)
+        {
+            if (element )
+            custom.Type("anything");
+            element = ReadOnly;
+            value = custom.GetValue(element);
+
+        }
+
+    }
+}
--- LetCode/Utils/CustomMethod.cs
+++ LetCode/Utils/CustomMethod.cs
@@ -1,10 +1,13 @@
 ﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+
 
 namespace LetCode.Utils
 {
     public class CustomMethod
     {
         private IWebDriver driver;
+        private Action action;
 
         public CustomMethod(IWebDriver driver)
         {
@@ -13,13 +16,29 @@ namespace LetCode.Utils
 
         public void ClickOn(IWebElement element) => element.Click();
 
-        public void Type(IWebElement element, string text) => element.SendKeys(text);
-
-        public string GetTitle(IWebElement element) 
+        public void Type(IWebElement element, string text)
+        {
+            element.Clear();
+            element.SendKeys(text);
+        };
+        public bool IsItEnabled(IWebElement element)
+        {
+            return element.Enabled;
+        }
+        public string GetTitle(IWebElement element)
         {
             string title = element.GetAttribute("title");
             return title;
         }
-
+        public string GetValue(IWebElement element)
+        {
+            string value = element.GetAttribute("value");
+            return value;
+        }
+        public void PressOn( string key)
+        {
+            Actions action = new Actions(driver);
+            action.SendKeys(key).Perform();
+        }
     }
 }
